fc755318ea28b6de834fc7b760e127bce341a749
Sh-wayz <shwayz03@gmail.com>
2020-11-20 22:05:28 -0500

    added list, empty dict file

---
+++ containers/list.py
@@ -0,0 +1,29 @@
+from copy import deepcopy
+
+class EnhancedList(list):
+
+    def __init__(self):
+        super().__init__()
+
+    def append(self, item):
+        """Appends and returns item."""
+
+        list.append(self, item)
+        return item
+
+    def indices(self, value):
+        """Returns all the indices of a given value"""
+        def indices_gen():
+            result = EnhancedList()
+            offset = -1
+            while True:
+                try:
+                    offset = self.index(value, offset+1)
+                except ValueError:
+                    return result
+                yield offset
+        return [*indices_gen()]
+    def copy(self):
+       """Returns a deep copy of the list"""
+       return deepcopy(self)
+    indexes = indices
