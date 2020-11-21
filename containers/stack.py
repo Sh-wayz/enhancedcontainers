8e733fc82944f2fe8038c8e208c57de9c672a182
Sh-wayz <shwayz03@gmail.com>
2020-11-20 23:59:06 -0500

    Allowed stack-ification of lists.

--- containers/stack.py
+++ containers/stack.py
@@ -1,26 +1,26 @@
 class Stack:
-    """LIFO(Last In First Out) data structure implemented with lists."""
+    """LIFO(Last In First Out) data structure. Implemented with lists."""
 
-    def __init__(self):
+    def __init__(self, _list=None):
 
-        self.items = []
+        self._items = ([] if _list is None else _list)
 
     def empty(self):
         """Returns whether or not the stack is empty"""
-        return self.items == []
+        return self._items == []
 
     def size(self):
         """Returns the total amount of items in stack."""
-        return len(self.items)
+        return len(self._items)
 
     def top(self):
         """Returns the value of the top item in the stack without removing."""
-        return self.items[-1]
+        return self._items[-1]
 
     def push(self, item):
         """Adds an item to the stack."""
-        return self.items.append(item)
+        return self._items.append(item)
 
     def pop(self):
         """Removes the top item in the stack."""
-        return self.items.pop()
+        return self._items.pop()
