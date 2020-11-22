1e2ff89e014e8b922c6fb10a9f7957dc6b3d8441
Sh-wayz <shwayz03@gmail.com>
2020-11-22 09:15:01 -0500

    Made imports less ew.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # enhanced-containers
+[![PyPI version fury.io](https://badge.fury.io/py/ansicolortags.svg)](https://pypi.python.org/pypi/enhancedcontainers)
+
 ![](https://api.ghprofile.me/view?username=sh-wayz-enhanced-containers&label=views&style=for-the-badge)
--- enhancedcontainers/__init__.py
+++ enhancedcontainers/__init__.py
@@ -1,4 +1,9 @@
 """Improved list and dict functions, plus queues and stacks."""
+from . import stack, queue, dict, list
 
+__version__ = '1.0.1'
 
-__version__ = '1.0.0'
+Stack = stack.Stack
+Queue = queue.Queue
+EnhancedDict = dict.EnhancedDict
+EnhancedList = list.EnhancedList
